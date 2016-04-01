275f63f3ef818295813e758f698221d561f35b7d
Bruno Ventura <brun0ventur4@gmail.com>
2016-03-31 23:36:01 -0300

    first commit

---
+++ static/lib/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import React, {Component} from 'react';
+import { render } from 'react-dom'
+import {bind} from 'decko';
+import CoursesContainer from './components/CoursesContainer';
+
+render(<CoursesContainer />, document.getElementById('root'));
---
+++ static/lib/components/CoursesContainer.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import 'whatwg-fetch';
+
+class CoursesContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            courses: []
+        }
+    }
+
+    componentWillMount() {
+        fetch('../courses.json')
+        .then(response => response.json())
+        .then(responseData => this.setState({courses: responseData}))
+        .catch(err => console.error(err));
+    }
+
+    render() {
+        const courseNames = this.state.courses.map(course => {
+            return (
+                <div className="col-md-3 col-sm-6 hero-feature">
+                    <div className="thumbnail">
+                        <div className="image-container" style={{backgroundImage: `url(${course.image})`}}></div>
+                        <div className="caption">
+                            <h3>{ course.name }</h3>
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h3>Todos Cursos</h3>
+                    </div>
+                </div>
+                <div className="row text-center">
+                    { courseNames }
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default CoursesContainer;
