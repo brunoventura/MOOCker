56364aad04cbd1abd5c473af3981d4bbdd81097a
Bruno Ventura <brun0ventur4@gmail.com>
2016-03-31 23:39:16 -0300

    Update 2016-04-01T02:39:12.311Z

--- static/lib/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import React, {Component} from 'react';
-import { render } from 'react-dom'
-import {bind} from 'decko';
-import CoursesContainer from './components/CoursesContainer';
-
-render(<CoursesContainer />, document.getElementById('root'));
